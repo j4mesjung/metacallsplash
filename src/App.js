b1cdf05fa37c466a18d6ceab6a0e262075c5b312
j4mesjung <jamesofjungle@gmail.com>
2017-07-18 21:48:57 -0700

    gogo

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="title">
+          <h1>M</h1>
+          <p>september 2017</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
